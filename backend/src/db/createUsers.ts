454f97d77a189831198636ad3adb09fbea83ec07
Teeraphat Raksawong <teeraphat_ra@cmu.ac.th>
2025-03-26 21:55:05 +0700

    logo design

--- backend/src/db/createUsers.ts
+++ backend/src/db/createUsers.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from '@prisma/client';
 async function createUsers(prisma: PrismaClient): Promise<void> {
     console.log('Seeding users...');
 
-    //password admin
+    //password | admin = $2a$12$4o6jPKch9kXKxKw9MkvvcuMUksRUD7kVMY7W/wQRGhlzVq7.VvriW
     const users = [
         { username: 'student001', password: '$2a$12$Co8IJcuyth2QRTaemFBw/Ow423ZbDFrQwWp4rUrwsZk6iQm51xLT2', role: 'Student' },
         { username: 'student002', password: '$2a$12$Co8IJcuyth2QRTaemFBw/Ow423ZbDFrQwWp4rUrwsZk6iQm51xLT2', role: 'Student' },
