b7e757bc24edfab75b08deebf882953e58774397
Teeraphat Raksawong <teeraphat_ra@cmu.ac.th>
2025-03-26 20:52:10 +0700

    add authen admin and advicsor

--- backend/src/routes/studentRoute.ts
+++ backend/src/routes/studentRoute.ts
@@ -1,10 +1,15 @@
-import express, { Response } from 'express';
-import { authenticateJWT } from '../middleware/authMiddleware'; // นำเข้า JWT Middleware
+import express, { Response, Request } from 'express';
+import { authenticateJWT, authorizeRole } from '../middleware/authMiddleware'; // นำเข้า JWT Middleware
+import { getStudent } from '../services/studentService'
 
 const router: express.Router = express.Router()
 
 // Route สำหรับดึงข้อมูลโปรไฟล์ผู้ใช้
 router.get('/profile', authenticateJWT, async (req: any, res: Response) => {
+
+  if (!req.user) {
+    return res.status(401).json({ message: 'User not authenticated' });
+  }
 
   const user = req.user; // ข้อมูลผู้ใช้ที่ได้จาก JWT token
 
@@ -14,5 +19,12 @@ router.get('/profile', authenticateJWT, async (req: any, res: Response) => {
     user,
   });
 });
+
+router.get('/', authenticateJWT, authorizeRole(['admin', 'advisor']), async (req: Request<any, any, any, any>, res: Response) => {
+  try {
+    const students = await getStudent(req.query?.search);
+    res.status(200).json({ data: students });
+  } catch (err: any) { res.status(500).json({ message: err?.message }); }
+})
 
 export default router;
